58a5e3c60547a807159f42abf8a3f0cd438fbedb
Anderson Barros Silva <anderson.tec12@gmail.com>
2024-02-01 08:42:23 -0300

    feat: configs initial express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Live Reload')
+import dotenv from 'dotenv';
+
+dotenv.config()
+import express from 'express';
+
+const app = express()
+
+app.use(express.json())
+
+app.use(express.urlencoded({
+  extended:true
+}))
+
+app.get('/', (req, res) => {
+  res.json({status:true})
+} )
+const port = process.env.PORT ?? 3001
+app.listen(port, () => {
+  console.log(`API is running in ${port}`)
+})
